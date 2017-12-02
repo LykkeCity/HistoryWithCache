f12c13fe1c376804b8a56943c29682e5044cd47e
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-12-03 02:27:44 +0300

    Added performance logging.

--- src/Lykke.Job.OperationsCache/PeriodicalHandlers/MyPeriodicalHandler.cs
+++ src/Lykke.Job.OperationsCache/PeriodicalHandlers/MyPeriodicalHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Async;
+using System.Linq;
 using System.Threading.Tasks;
 using Common;
 using Common.Log;
@@ -9,6 +10,7 @@ namespace Lykke.Job.OperationsCache.PeriodicalHandlers
 {
     public class MyPeriodicalHandler : TimerPeriod
     {
+        private readonly ILog _log;
         private readonly IHistoryCache _historyCache;
         private readonly ClientSessionsRepository _clientSessionsRepository;
         private static bool _inProcess;
@@ -20,6 +22,7 @@ namespace Lykke.Job.OperationsCache.PeriodicalHandlers
             TimeSpan expirationPeriod) :
             base(nameof(MyPeriodicalHandler), (int)expirationPeriod.TotalMilliseconds, log)
         {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
             _clientSessionsRepository = clientSessionsRepository ?? throw new ArgumentNullException(nameof(_clientSessionsRepository));
             _historyCache = historyCache ?? throw new ArgumentNullException(nameof(historyCache));
         }
@@ -33,12 +36,14 @@ namespace Lykke.Job.OperationsCache.PeriodicalHandlers
             {
                 _inProcess = true;
 
-                var clientsIds = await _clientSessionsRepository.GetClientsIds();
-
+                var timestamp = DateTime.UtcNow;
+                var clientsIds = (await _clientSessionsRepository.GetClientsIds()).ToList();
                 await clientsIds.ParallelForEachAsync(async clientId =>
                 {
                     await _historyCache.WarmUp(clientId).ConfigureAwait(false);
                 }).ConfigureAwait(false);
+
+                await _log.WriteInfoAsync(GetComponentName(), "Updating cache", $"Processed {clientsIds.Count} active clients in {(DateTime.UtcNow - timestamp).TotalSeconds} seconds");
             }
             finally
             {
