e7c11522722b69a9c08e1620c15a4c7ce503bb75
atarutin <tarutin_andrew@mail.ru>
2017-12-03 17:39:50 +0300

    added nuget client project

---
+++ client/Lykke.Service.OperationsCache.Client/Models/HistoryClientEntry.cs
@@ -0,0 +1,32 @@
+﻿using Lykke.Service.OperationsCache.AutorestClient.Models;
+
+namespace Lykke.Service.OperationsCache.Client.Models
+{
+    public class HistoryClientEntry
+    {
+        public string Id { get; set; }
+        public System.DateTime? DateTime { get; set; }
+        public double? Amount { get; set; }
+        public string Currency { get; set; }
+        public string ClientId { get; set; }
+        public string CustomData { get; set; }
+        public string OpType { get; set; }
+    }
+
+    public static class Mapper
+    {
+        public static HistoryClientEntry FromApiModel(this HistoryEntry model)
+        {
+            return new HistoryClientEntry
+            {
+                Id = model.Id,
+                Amount = model.Amount,
+                ClientId = model.ClientId,
+                Currency = model.Currency,
+                CustomData = model.CustomData,
+                DateTime = model.DateTime,
+                OpType = model.OpType
+            };
+        }
+    }
+}
