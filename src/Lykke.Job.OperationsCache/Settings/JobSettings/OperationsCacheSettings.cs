24318b8ae5eb45e50aaba6f56b5fa53fd66570e6
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-12-23 19:41:06 +0300

    Changed "CacheInstanceName" property as Required.

--- src/Lykke.Job.OperationsCache/Settings/JobSettings/OperationsCacheSettings.cs
+++ src/Lykke.Job.OperationsCache/Settings/JobSettings/OperationsCacheSettings.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Lykke.SettingsReader.Attributes;
 
 namespace Lykke.Job.OperationsCache.Settings.JobSettings
 {
     public class OperationsCacheSettings
     {
-        [Optional]
         public string CacheInstanceName { get; set; }
         public TimeSpan ExpirationPeriod { get; set; }
         public DbSettings Db { get; set; }
