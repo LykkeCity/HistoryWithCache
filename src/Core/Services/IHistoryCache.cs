3095e01bab74d6ac5b611d0e81e3fd365d8f2980
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-02-26 21:48:54 +0300

    Revert "Merge branch 'feature_LWDEV-5576-split-service' into dev"
    This reverts commit 4992cb10aa4efb476c34f4ea34d47d73993d9410, reversing
    changes made to e0fe44b5291d6dda0fe041afe59a015b6edbc4a1.

--- src/Core/Services/IHistoryCache.cs
+++ src/Core/Services/IHistoryCache.cs
@@ -7,8 +7,6 @@ namespace Core.Services
     public interface IHistoryCache
     {
         Task<IEnumerable<HistoryEntry>> GetAllPagedAsync(string clientId, int page);
-
-        Task<IEnumerable<HistoryEntry>> GetRecordsByClient(string clientId);
         
         Task WarmUp(string clientId, bool force = false);
     }
